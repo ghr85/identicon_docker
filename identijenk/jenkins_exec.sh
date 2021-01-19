ec454681820ea2a1492315b745044e88be1b01b1
ghr85 <ghrenfrew@gmail.com>
2021-01-19 08:13:43 +0000

    updated variables in jenkins script

--- identijenk/jenkins_exec.sh
+++ identijenk/jenkins_exec.sh
@@ -14,8 +14,8 @@ sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock ERR=
 
 # Run system test if unit tests passed, inspect container to obtain IP address, curl identidock service to determine if 200 response
 if [ $ERR -eq 0 ]; then
-	$IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} jenkins_identidock_1)
-	$CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
+	IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} jenkins_identidock_1)
+	CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
 	if [ $CODE -ne 200 ]; then
 		echo 'site returned' $CODE
     	ERR=1
