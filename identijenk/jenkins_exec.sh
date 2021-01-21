675757ed542545bb4eb2a68d409ca26c1646c106
ghr85 <ghrenfrew@gmail.com>
2021-01-21 05:43:04 +0000

    updated script to auto tag repository and push

--- identijenk/jenkins_exec.sh
+++ identijenk/jenkins_exec.sh
@@ -14,9 +14,21 @@ sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock ERR=
 
 # Run system test if unit tests passed, inspect container to obtain IP address, curl identidock service to determine if 200 response
 if [ $ERR -eq 0 ]; then
+# Get the IP of the container
 	IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} jenkins_identidock_1)
+    # Ping the container and test for 200 status note variable set so as assingment works
 	CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
-	if [ $CODE -ne 200 ]; then
+	if [ $CODE -e 200 ]; then
+        echo 'System test passed - now tagging image'
+        # get the head of the latest commit
+        HASH=$(git rev-parse --short HEAD)
+        sudo docker tag -f jenkins_identidock ghruoa/identidock:$HASH
+        sudo docker tag -f jenkins_identidock ghruoa/identidock:newest
+        echo 'Pushing tagged image'
+        sudo docker login -e $1 -u $2 -p $3
+        sudo docker push ghruoa/identidock:$HASH
+        sudo docker push ghruoa/identidock:newest
+    else
 		echo 'site returned' $CODE
     	ERR=1
 	fi
